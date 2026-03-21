3e65ef0f9b544d82ef7322995f3939fe742e251f
Tristan Gautie <tristan@zml.ai>
2026-03-21 16:51:23 +0100

    Introduce first application: shell

--- run.sh
+++ run.sh
@@ -4,12 +4,20 @@ set -xue
 # QEMU file path
 QEMU=qemu-system-riscv32
 
+# objcopy file path
+OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
+
 # Path to clang and compiler flags
 CC=/opt/homebrew/opt/llvm/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
+# Build the shell (application)
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 # Build the kernel
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c shell.bin.o
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
