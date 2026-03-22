23cbeb2b28c888a4d4576abf6e99c9b2b5c8bd36
Tristan Gautie <tristan@zml.ai>
2026-03-22 12:09:45 +0100

    Simple shell

--- kernel.c
+++ kernel.c
@@ -10,81 +10,6 @@ struct process procs[PROCS_MAX]; // All process control structures.
 struct process *current_proc;    // Currently running process
 struct process *idle_proc;       // Idle process
 
-struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
-                       long arg5, long fid, long eid)
-{
-    register long a0 __asm__("a0") = arg0;
-    register long a1 __asm__("a1") = arg1;
-    register long a2 __asm__("a2") = arg2;
-    register long a3 __asm__("a3") = arg3;
-    register long a4 __asm__("a4") = arg4;
-    register long a5 __asm__("a5") = arg5;
-    register long a6 __asm__("a6") = fid;
-    register long a7 __asm__("a7") = eid;
-
-    __asm__ __volatile__("ecall"
-                         : "=r"(a0), "=r"(a1)
-                         : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
-                           "r"(a6), "r"(a7)
-                         : "memory");
-    return (struct sbiret){.error = a0, .value = a1};
-}
-
-void putchar(char ch)
-{
-    sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
-}
-
-long getchar(void)
-{
-    struct sbiret ret = sbi_call(0, 0, 0, 0, 0, 0, 0, 2 /* Console Getchar */);
-    return ret.error;
-}
-
-void handle_syscall(struct trap_frame *f)
-{
-    switch (f->a3)
-    {
-    case SYS_PUTCHAR:
-        putchar(f->a0);
-        break;
-    case SYS_GETCHAR:
-        while (1)
-        {
-            long ch = getchar();
-            if (ch >= 0)
-            {
-                f->a0 = ch;
-                break;
-            }
-
-            yield();
-        }
-        break;
-    default:
-        PANIC("unexpected syscall a3=%x\n", f->a3);
-    }
-}
-
-void handle_trap(struct trap_frame *f)
-{
-    uint32_t scause = READ_CSR(scause);
-    uint32_t stval = READ_CSR(stval);
-    uint32_t user_pc = READ_CSR(sepc);
-
-    if (scause == SCAUSE_ECALL)
-    {
-        handle_syscall(f);
-        user_pc += 4;
-    }
-    else
-    {
-        PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
-    }
-
-    WRITE_CSR(sepc, user_pc);
-}
-
 paddr_t alloc_pages(uint32_t n)
 {
     static paddr_t next_paddr = (paddr_t)__free_ram;
@@ -387,4 +312,79 @@ void kernel_main(void)
 
     yield();
     PANIC("unreachable here!");
+}
+
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
+                       long arg5, long fid, long eid)
+{
+    register long a0 __asm__("a0") = arg0;
+    register long a1 __asm__("a1") = arg1;
+    register long a2 __asm__("a2") = arg2;
+    register long a3 __asm__("a3") = arg3;
+    register long a4 __asm__("a4") = arg4;
+    register long a5 __asm__("a5") = arg5;
+    register long a6 __asm__("a6") = fid;
+    register long a7 __asm__("a7") = eid;
+
+    __asm__ __volatile__("ecall"
+                         : "=r"(a0), "=r"(a1)
+                         : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
+                           "r"(a6), "r"(a7)
+                         : "memory");
+    return (struct sbiret){.error = a0, .value = a1};
+}
+
+void putchar(char ch)
+{
+    sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
+}
+
+long getchar(void)
+{
+    struct sbiret ret = sbi_call(0, 0, 0, 0, 0, 0, 0, 2 /* Console Getchar */);
+    return ret.error;
+}
+
+void handle_syscall(struct trap_frame *f)
+{
+    switch (f->a3)
+    {
+    case SYS_PUTCHAR:
+        putchar(f->a0);
+        break;
+    case SYS_GETCHAR:
+        while (1)
+        {
+            long ch = getchar();
+            if (ch >= 0)
+            {
+                f->a0 = ch;
+                break;
+            }
+
+            yield();
+        }
+        break;
+    default:
+        PANIC("unexpected syscall a3=%x\n", f->a3);
+    }
+}
+
+void handle_trap(struct trap_frame *f)
+{
+    uint32_t scause = READ_CSR(scause);
+    uint32_t stval = READ_CSR(stval);
+    uint32_t user_pc = READ_CSR(sepc);
+
+    if (scause == SCAUSE_ECALL)
+    {
+        handle_syscall(f);
+        user_pc += 4;
+    }
+    else
+    {
+        PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
+    }
+
+    WRITE_CSR(sepc, user_pc);
 }
