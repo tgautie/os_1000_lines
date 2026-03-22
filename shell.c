4ffe355b77272a712aedd62f30b249a9c29ba19f
Tristan Gautie <tristan@zml.ai>
2026-03-22 11:56:27 +0100

    Syscall setup

--- shell.c
+++ shell.c
@@ -2,7 +2,5 @@
 
 void main(void)
 {
-    *((volatile int *)0x80200000) = 0x1234; // Forbidden write to kernel owned memory, causes a "Store/AMO page fault" (scause=15)
-    for (;;)
-        ;
+    printf("Hello World from shell!\n");
 }
